1269c0593cefbbae94a932d85e64964436bc1ecb
Aingty Eung <aingtyeung@yahoo.com>
2018-01-25 21:50:07 -0800

    Progress on assignment 1 Just finished the recursive and menu and input validation

--- Assignment-1-Programming_Review/Main.cpp
+++ Assignment-1-Programming_Review/Main.cpp
@@ -1,9 +1,8 @@
 
 #include<iostream>
-#include<iomanip>
-#include<math.h>
 using namespace std;
 
+int recursiveFunction(int);
 
 int main()
 {
@@ -12,25 +11,43 @@ int main()
     cout << "Welcome to the Array Structure Program!" << endl;
     while (keepGoing)
     {
-        cout << "Please select an option:\t1. Access a Pointer\n\t2. List Deallocated Memory (index)\n\t3. Deallocate All Memory\n\t4. Exit Program"<<endl;
+        cout << "Please select an option:\n\t1. Access a Pointer\n\t2. List Deallocated Memory (index)\n\t3. Deallocate All Memory\n\t4. Exit Program"<<endl;
         cin >> decision;
         for (int i = 0; i < decision.length(); i++)
         {
             char c = decision[i];
-            if (!c.isdigit())
+            if (!isdigit(c))
             {
                 keepGoing = false;
             }
         }
         if (keepGoing)
         {
-            cout << "success" << endl;   
+            int arrayInteger [20];
+            char* arrayChar[20];
+            for (int i = 0; i < sizeof(arrayInteger); i++)
+            {
+                arrayInteger[i] = recursiveFunction(i);
+            }
+               
         }
         else
         {
             cout << "fail" << endl;
         }
     }
+}
 
-
+/*--------------------------------------------*/
+// Given recursive function
+int recursiveFunction(int i)
+{
+    if (i == 0)
+    {
+        return 2700;
+    }
+    else
+    {
+        return recursiveFunction(i-1) * 2;
+    }
 }
