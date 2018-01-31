927b61fb5d9e79f451a99b4a18260a9918b3cf3d
Aingty Eung <aingtyeung@yahoo.com>
2018-01-30 20:18:13 -0800

    Finished the Menu System

--- Assignment-1-Programming_Review/Main.cpp
+++ Assignment-1-Programming_Review/Main.cpp
@@ -11,29 +11,62 @@ int main()
     cout << "Welcome to the Array Structure Program!" << endl;
     while (keepGoing)
     {
-        cout << "Please select an option:\n\t1. Access a Pointer\n\t2. List Deallocated Memory (index)\n\t3. Deallocate All Memory\n\t4. Exit Program"<<endl;
+        cout << "Please select an option:\n\t1. Access a Pointer\n\t2. List Deallocated Memory (index)\n\t3. Deallocate All Memory\n\t4. Exit Program\nYour input: ";
         cin >> decision;
+        cout << "\n";
         for (int i = 0; i < decision.length(); i++)
         {
             char c = decision[i];
             if (!isdigit(c))
             {
                 keepGoing = false;
+                break;
             }
         }
         if (keepGoing)
         {
-            int arrayInteger [20];
-            char* arrayChar[20];
-            for (int i = 0; i < sizeof(arrayInteger); i++)
+            int trueDecision = stoi(decision);
+            // int arrayInteger [20];
+            // char* arrayChar[20];
+            // for (int i = 0; i < sizeof(arrayInteger); i++)
+            // {
+            //     arrayInteger[i] = recursiveFunction(i);
+            // }
+            switch (trueDecision)
             {
-                arrayInteger[i] = recursiveFunction(i);
-            }
-               
+                case 1:
+                {
+
+                    break;
+                }
+                case 2:
+                {
+
+                    break;
+                }
+                case 3:
+                {
+
+                    break;
+                }
+                case 4:
+                {
+                    cout << "Thank you for using the program. GoodBye! :)" << endl;
+                    keepGoing = false;
+                    break;
+                }
+                default: 
+                {
+                    cout << "Invalid input!!! please choose one of the 4 options!" << endl;
+                    break;
+                }
+
+            }  
         }
         else
         {
-            cout << "fail" << endl;
+            cout << "Invalid input!!! Input must be a number!" << endl;
+            keepGoing = true;
         }
     }
 }
